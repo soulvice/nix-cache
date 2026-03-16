bf0a980124b36e70cf6c4254cbd39040ca0c45c3
soulvice <admin@w0lf.io>
2026-03-16 14:03:50 +1000

    Initial Commit. Current Builds are: - Niri (both custom and mainline) - Vicinae - Vicinae Extensions (soulvice) - Ghostty

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # -- Niri --------------------------- 
+    # --------------------------------------------
+    niri-custom = {
+      url = "github:soulvice/niri";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri = {
+      url = "github:niri-wm/niri";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # -- Vicinae & Extensions --------------------
+    # --------------------------------------------
+    vicinae = {
+      url = "github:vicinaehq/vicinae";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    vicinae-extensions-soulvice = {
+      url = "github:soulvice/vicinae-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # -- Ghostty Terminal ------------------------
+    # --------------------------------------------
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, niri, niri-custom, vicinae, vicinae-extensions-soulvice, ghostty,  ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system} = {
+        niri = {
+          default = niri-custom.packages.${system}.niri;
+          mainline = niri.packages.${system}.niri;
+        };
+
+        vicinae = {
+          default = vicinae.packages.${system}.vicinae;
+          extensions = {
+            soulvice = vicinae-extensions-soulvice.packages.${system};
+          };
+        };
+
+        ghostty = ghostty.packages.${system}.default;
+
+        # TODO: add more packages here
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ ];
+      };
+    };
+}
